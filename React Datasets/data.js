56e801f7714c746038a94966a8b888803a639834
HarounTheGreat <harounbouriga99@gmail.com>
2022-12-30 00:13:05 +0100

    Blood-Donation-Project

---
+++ React Datasets/data.js
@@ -0,0 +1,11 @@
+const data = {
+  id: "12858056",
+  fullName: "mohamed haroun",
+  sex: "M",
+  dateofbirth: "1999/24/12",
+  blood_type: "A+",
+  city: "MSAKEN",
+  zipcode: 4070,
+  phoneNum: "93234428",
+  last_donation: "2022/29/12",
+};
